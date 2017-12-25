36a5aca3b45fa6e8e2d896a3bdb239bb147fc93a
Zinc <zinc@Zinc.local>
2017-12-25 09:40:06 +0800

    add file

---
+++ num.py
@@ -0,0 +1,25 @@
+f=70.794
+f1=45.972
+f2=24.822
+f3=4.662
+f4=26.712
+
+
+while True:
+    print '#############################'
+    num1=input('please input num1: \n')
+    num2=input('please input num2: \n')
+    num3=input('please input num3: \n')
+    a=f*num1
+    b1=f1*num1+f2*num2
+    b2=f1*num1+f2*num3
+    c1=f3*num1+f4*num2
+    c2=f3*num1+f4*num3
+#    m=max(a,b1,b2,c1,c2)
+    print '#############################'
+    print a
+    print b1
+    print b2
+    print c1
+    print c2
+#   print m
