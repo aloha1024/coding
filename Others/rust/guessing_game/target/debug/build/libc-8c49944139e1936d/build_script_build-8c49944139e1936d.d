955931b82349b193a140468d3510bd3184de1d5c
aloha1024 <aloha1024@protonmail.com>
2019-08-12 14:14:43 +0800

    update

---
+++ Others/rust/guessing_game/target/debug/build/libc-8c49944139e1936d/build_script_build-8c49944139e1936d.d
@@ -0,0 +1,5 @@
+/home/zinc/coding/Others/rust/guessing_game/target/debug/build/libc-8c49944139e1936d/build_script_build-8c49944139e1936d: /home/zinc/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs
+
+/home/zinc/coding/Others/rust/guessing_game/target/debug/build/libc-8c49944139e1936d/build_script_build-8c49944139e1936d.d: /home/zinc/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs
+
+/home/zinc/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs:
--- Others/rust/guessing_game/target/debug/guessing_game.d
+++ Others/rust/guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Users/zinc/Files/coding/Others/rust/guessing_game/target/debug/guessing_game.dSYM: /Users/zinc/Files/coding/Others/rust/guessing_game/src/main.rs
+/home/zinc/coding/Others/rust/guessing_game/target/debug/guessing_game: /home/zinc/coding/Others/rust/guessing_game/src/main.rs
