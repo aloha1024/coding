e26b2f1d91d9c0140f6de14af0d4fc9199d442d6
aloha1024 <aloha1024@protonmail.com>
2019-08-16 20:12:34 +0800

    add

---
+++ Others/rust/example/src/main.rs
@@ -0,0 +1,56 @@
+
+
+fn main() {
+    println!("Hello, world!");
+
+}
+
+fn example1 {
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    }else{
+        println!("condition was false");
+    }
+}
+
+//This is a wrong example
+// fn example2 {
+//     let number = 3;
+//     if number {
+//         println!("number was three");
+//     }
+// }
+
+
+//use "if" in "let"
+fn example3 {
+    let condition = true;
+    let number = if condition {
+        5
+    }else{
+        6
+    };
+    println!("the value of number is : {}",number);
+}
+
+fn example4 {
+    loop {
+        println!("again!");
+    }
+}
+
+
+fn example5 {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
